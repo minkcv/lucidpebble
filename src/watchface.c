4b39f1c18d1171bd4dce9cc009a639e1ef6b1ba6
minkcv <mink.cv@gmail.com>
2016-06-02 15:17:12 -0600

    finished tutorial part 2

--- src/watchface.c
+++ src/watchface.c
@@ -2,20 +2,36 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void main_window_load(Window *window) {
     // Get info about the Window
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
 
+    // Create GBitmap
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+
+    // Create BitmapLayer
+    s_background_layer = bitmap_layer_create(bounds);
+
+    // Set the bitmap onto the layer and add to the window
+    bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+    layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
     // Create the text layer with bounds
     s_time_layer = text_layer_create(
             GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
+    // Load font
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+
     // Improve layout
     text_layer_set_background_color(s_time_layer, GColorClear);
-    text_layer_set_text_color(s_time_layer, GColorBlack);
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_text_color(s_time_layer, GColorWhite);
+    text_layer_set_font(s_time_layer, s_time_font);
     text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
     // Add as child layer to window
@@ -25,6 +41,15 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
     // Destroy TextLayer
     text_layer_destroy(s_time_layer);
+
+    // Unload font
+    fonts_unload_custom_font(s_time_font);
+
+    // Destroy GBitmap
+    gbitmap_destroy(s_background_bitmap);
+
+    // Destory BitmapLayer
+    bitmap_layer_destroy(s_background_layer);
 }
 
 static void update_time() {
@@ -54,6 +79,8 @@ static void init() {
             .load = main_window_load,
             .unload = main_window_unload
     });
+
+    window_set_background_color(s_main_window, GColorBlack);
 
     // Show the Window on the watch, with animated=true
     window_stack_push(s_main_window, true);
